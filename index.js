6c0fbd4291b5694226e0ef8c288779b532af054a
Charlotte Keitumetse Lekoro <156782118+CharlotteLekoro@users.noreply.github.com>
2024-05-07 12:55:15 +0200

    fuel calculation
    This code is used to know how much fuel is left after a certain duration of burning, as it considers the initial fuel level.
    Rearranged the order of the variable names for the calculation of new velocity

--- index.js
+++ index.js
@@ -13,17 +13,15 @@ const initialDistance = 0; // distance (km)
 const initialFuel = 5000; // remaining fuel (kg)
 const fuelBurnRate = 0.5; // fuel burn rate (kg/s)
 
+calculateNewVelocity = (velocity, acceleration, timeInHours) => { 
+  return velocity + (acceleration*timeInHours)
+}
 // Convert time to hours
 const timeInHours = timeInSeconds / 3600;
 
 const newDistance = initialDistance + (velocity*timeInHours) //calcultes new distance
-const remainingFuelAfterBurn = fuelBurnRate*timeInHours //calculates remaining fuel
-const newVelocity = calculateNewVelocity(acceleration, velocity, timeInHours) //calculates new velocity based on acceleration
-
-// Pick up an error with how the function below is called and make it robust to such errors
-calculateNewVelocity = (velocity, acceleration, timeInHours) => { 
-  return velocity + (acceleration*timeInHours)
-}
+const remainingFuelAfterBurn = remainingFuel - (fuelBurnRate * timeInHours); //calculates remaining fuel
+const newVelocity = calculateNewVelocity(velocity, acceleration, timeInHours) //calculates new velocity based on acceleration
 
 console.log(`Corrected New Velocity: ${newVelocity} km/h`);
 console.log(`Corrected New Distance: ${newDistance} km`);
